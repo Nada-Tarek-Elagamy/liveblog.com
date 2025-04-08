d4af5db1dd663e362cfe30fa1599adf4e0d63e63
Nada Tarek Elagamy <nadatarekesmatelagamy@gmail.com>
2025-04-08 15:01:36 +0200

    edited2

--- app/page.js
+++ app/page.js
@@ -67,7 +67,7 @@ const HomePage = () => {
 
   useEffect(() => {
     if (user) {
-      const q = query(collection(db, "posts");
+      const q = query(collection(db, "posts"),
       const unsubscribe = onSnapshot(q, (snapshot) => {
         setPosts(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
       });
