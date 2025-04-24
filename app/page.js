7b57c0a6e03ec2d67b6d911844e5a60242500911
Nada Tarek Elagamy <nadatarekesmatelagamy@gmail.com>
2025-04-24 22:49:41 +0200

    ad

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { auth } from "../lib/firebaseConfig";
+import { getFirestore } from "firebase/firestore"; // Ensure this import is correct
 import { db } from "../lib/firebaseConfig";
 import { collection } from "firebase/firestore";
 
